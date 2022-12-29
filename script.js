dce82e7b5a838e09f2780c6e2fe763d07df79cd5
ariel1031 <ariel1031@daum.net>
2022-12-30 01:27:59 +0900

    삭제버튼

--- script.js
+++ script.js
@@ -13,10 +13,20 @@ const addList = (nickname, comment) => {
     const li = document.createElement('li')
     const nick = document.createElement('span')
     const com = document.createElement('span')
+    const deleteButton = document.createElement('button')
+
     nick.innerText = nickname
     com.innerText = comment
+    deleteButton.innerText = '삭제'
 
+    const deleteFunc = (e) => {
+        console.log('e.currentTarget.parentNode : ', e.currentTarget.parentNode)
+        e.currentTarget.parentNode.remove() //현재 타겟의 부모노드, 즉 li?
+    }
+
+    deleteButton.addEventListener('click', deleteFunc)
     li.append(nick)
     li.append(com)
+    li.append(deleteButton)
     commentList.append(li)
 }
