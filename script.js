59bb75dee4cb88957aa97f6d01674940a0a24ba0
ariel1031 <ariel1031@daum.net>
2022-12-30 22:26:21 +0900

    수정 버튼 작업

--- script.js
+++ script.js
@@ -13,20 +13,47 @@ const addList = (nickname, comment) => {
     const li = document.createElement('li')
     const nick = document.createElement('span')
     const com = document.createElement('span')
+    const editButton = document.createElement('button')
     const deleteButton = document.createElement('button')
 
-    nick.innerText = nickname
+    nick.innerText = nickname + ' '
     com.innerText = comment
+    editButton.innerText = '수정'
     deleteButton.innerText = '삭제'
 
+    const editFunc = (e) => {
+        //console.log('comment', comment)
+        console.log('e.currentTarget', e.currentTarget)
+        console.log('e.currentTarget.parentNode', e.currentTarget.parentNode)
+        e.currentTarget.parentNode.remove() //삭제가 안되는 이유를 모르겠음
+        const nick = document.createElement('span')
+        const editInput = document.createElement('input')
+        const editConfirm = document.createElement('button')
+        const deleteButton = document.createElement('button')
+        nick.innerText = nickname
+        //nick.tagName
+        editInput.type = 'text'
+        editInput.value = comment
+        editConfirm.innerText = '수정 확인'
+        deleteButton.innerText = '삭제'
+        editConfirm.addEventListener('click', handleSubmit)
+
+        li.append(nick)
+        li.append(editInput)
+        li.append(editConfirm)
+        li.append(deleteButton)
+        commentList.append(li)
+    }
     const deleteFunc = (e) => {
         console.log('e.currentTarget.parentNode : ', e.currentTarget.parentNode)
         e.currentTarget.parentNode.remove() //현재 타겟의 부모노드, 즉 li?
     }
 
+    editButton.addEventListener('click', editFunc)
     deleteButton.addEventListener('click', deleteFunc)
     li.append(nick)
     li.append(com)
+    li.append(editButton)
     li.append(deleteButton)
     commentList.append(li)
 }
